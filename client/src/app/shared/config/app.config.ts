8ebd130bcc32adaab8a328c973b40c23a02de5dc
devupadhyayrocker <devupadhyay200@gmail.com>
2020-10-19 18:51:20 +0530

    data updated

--- client/src/app/shared/config/app.config.ts
+++ client/src/app/shared/config/app.config.ts
@@ -1,11 +1,11 @@
 export class AppConfig {
-    public static commonurl = 'http://localhost:3000/api/';
+    public static commonurl = 'http://192.168.1.77:3000/api/';
     public static Subjects = ["English", "E.V.S.", "Social Studies", "Hindi", "Mathematics", "Computer", "G.K.", "Physics", "Chemistry", "Biology", "Music", "Physical Education", "Geography", "Sanskrit", "Political Science", "History", "Dance", "Art & Craft", "French", "Gurmukhi", "German"];
     public static schools = ["Seth Anandram Jaipuria School, Unnao", "Seth Anandram Jaipuria School, Lucknow (Alambagh Extn)", "Seth Anandram Jaipuria School, Varanasi",
         "Seth Anandram Jaipuria School, Sitarganj", "Seth Anandram Jaipuria School, Satna", "Seth Anandram Jaipuria School, Raebareli", "Seth Anandram Jaipuria School, Mirzapur",
         "Seth Anandram Jaipuria School, Dibiyapur"];
     public static staffType = ["Teaching", "Non-Teaching"];
-    public static nonteachingArea = ["Admin", "Transport", "Maintenance", "Counsellor", "Back Office Executive", "M.E."];
+    public static nonteachingArea = ["Admin", "Accounts", "Transport", "Maintenance", "Counsellor", "Back Office Executive", "M.E."];
     public static teacherType = ["Scholastic", "Co-Scholastic"];
     public static teacherCategory = ["N.T.", "P.R.T.", "T.G.T.", "P.G.T."];
     public static classTeacher = ["Yes", "No"];
@@ -15,11 +15,11 @@ export class AppConfig {
     public static TeachingFormList = ["Self Evaluation", "Peer Evaluation", "Document Evaluation"];
     public static nt_class = ["Pre-Nursery/Nursery", "L.K.G./Nursery", "U.K.G./Prep"];
     public static prt_class = ["Class 1", "Class 2", "Class 3", "Class 4", "Class 5"];
-    public static tgt_class = ["Class 6", "Class 7", "Class 8"];
-    public static pgt_class = ["Class 9", "Class 10", "Class 11", "Class 12"];
-    public static nt_sub = ["English", "E.V.S.", "Hindi", "Mathematics"];
-    public static prt_sub = ["English", "E.V.S./Social Studies", "Hindi", "Mathematics","Science","Sanskrit","G.K.","Art","Computer"];
-    public static tgt_sub = ["English", "Hindi", "Mathematics", "Physics", "Chemistry", "Biology",  "Geography","History",  "French",  "German","Sanskrit"];
+    public static tgt_class = ["Class 4", "Class 5", "Class 6", "Class 7", "Class 8", "Class 9", "Class 10"];
+    public static pgt_class = ["Class 11", "Class 12"];
+    public static nt_sub = ["English", "E.V.S.", "Hindi", "Mathematics", "Music", "Dance/Activity"];
+    public static prt_sub = ["English", "E.V.S./Social Studies", "Hindi", "Mathematics", "Science", "Sanskrit", "G.K.", "Art", "Computer"];
+    public static tgt_sub = ["English", "Hindi", "Mathematics", "Physics", "Chemistry", "Biology", "Science", "Social Studies", "Geography", "History", "French", "German", "Sanskrit"];
     public static pgt_sub = ["English", "E.V.S."];
     public static selectStaff = [{
         id: 1,
